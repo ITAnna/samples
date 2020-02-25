27d821a1dd07e34533f4687d42ab1fcd9398078e
ITAnna <zimanya@gmail.com>
2020-02-25 22:40:59 +0300

    the descending sorting is added

--- src/com/mysamples/MasBubbleSorting.java
+++ src/com/mysamples/MasBubbleSorting.java
@@ -22,7 +22,7 @@ public class MasBubbleSorting {
     }
 
 
-    private void sortMas() { //the method created
+    private void sortMasAsc() { //the method created
         boolean swap; //flag set to false by default
         int last = mas.length - 1; //last checked element is smaller cause it is compared with last element
         do {
@@ -37,19 +37,43 @@ public class MasBubbleSorting {
                 }
             }
             last--; //because maximum element always takes the last place
-        } while (swap == true);
+        } while (swap);
         System.out.println();
-        for (int i = 0; i < mas.length; i++) {
-            System.out.print(" "+ mas[i] + " "); //here we print the sorted massive elements
+        for (int i : mas) {
+            System.out.print(" " + mas[i] + " "); //here we print the sorted massive elements
         }
+
         System.out.print("The massive is sorted in ascending order");
+    }
+
+    private void sortMasDesc() { //the method created
+        boolean swap; //flag set to false by default
+        int last = mas.length - 1; //last checked element is smaller cause it is compared with last element
+        do {
+            swap = false;
+            int buffer; //temporary int to change 2 elements places
+            for (int i = 0; i < last; i++) {
+                if (mas[i] < mas[i + 1]) { //the condition to replace elements: if next element is bigger, then replace
+                    buffer = mas[i]; //save the element
+                    mas[i] = mas[i + 1]; //the bigger element take the previous place
+                    mas[i + 1] = buffer; //the saved element take the next place
+                    swap = true; //if some elements has been replaced, the flag becomes true, if doesn't then it stays in the false stage and cycle stops.
+                }
+            }
+            last--; //because maximum element always takes the last place
+        } while (swap);
+        System.out.println();
+        for (int i : mas) {
+            System.out.print(" " + mas[i] + " "); //here we print the sorted massive elements
+        }
+        System.out.print("The massive is sorted in descending order");
     }
 
 
     public static void main(String[] args) {
         MasBubbleSorting mas = new MasBubbleSorting();
         mas.fillMas(); //the filling method is invoked
-        mas.sortMas(); //the sorting method is invoked
+        mas.sortMasAsc(); //the ascending sorting method is invoked
+        mas.sortMasDesc(); //the descending sorting method is invoked
     }
-
 }
